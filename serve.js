97369a1f40085abf34bcf38efefe6832158add5f
Mohammed Odunayo <factman60@gmail.com>
2019-02-04 15:47:29 +0100

    Deployment

--- serve.js
+++ serve.js
@@ -3,11 +3,13 @@ express = require('express');
 const path = require('path');
 const app = express();
 
+try {
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/joke-frontend'));
+app.use(express.static(__dirname + '/dist/jokes-frontend'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/joke-frontend/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/jokes-frontend/index.html'));
 });
+} catch (ex => console.log(ex.message));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
